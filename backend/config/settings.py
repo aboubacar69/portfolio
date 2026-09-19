cfe0a4998f28790434cb27165b2b06dbb0e2e808
Said Mouinou Aboubacar <saidmouinou.aboubacar@ucad.edu.sn>
2026-09-19 02:32:57 +0100

    Modification de settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,6 +78,11 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
+        'CONN_MAX_AGE': 60,
+        'CONN_HEALTH_CHECKS': True,
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE', default='prefer'),
+        },
     }
 }
 
